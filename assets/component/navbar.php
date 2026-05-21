a6d647aa67f8c38edd899cc28e72b981c7a14f6e
abidzar220672 <gifariabidzar1@gmail.com>
2026-05-21 11:30:32 +0700

    fix

--- assets/component/navbar.php
+++ assets/component/navbar.php
@@ -1,8 +1,8 @@
 <ul class="dropdown-menu">
-  <li><a href="men.php" class="<?= basename($_SERVER['PHP_SELF']) == 'men.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Men</button></a></li>
-  <li><a href="kids.php" class="<?= basename($_SERVER['PHP_SELF']) == 'kids.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Kids</button></a></li>
-  <li><a href="new.php" class="<?= basename($_SERVER['PHP_SELF']) == 'new.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">New</button></a></li>
-  <li><a href="women.php" class="<?= basename($_SERVER['PHP_SELF']) == 'women.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Women</button></a></li>
-  <li><a href="shopall.php" class="<?= basename($_SERVER['PHP_SELF']) == 'shopall.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Shop All</button></a></li>
+  <li><a href="assets/page/men.php" class="<?= basename($_SERVER['PHP_SELF']) == 'men.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Men</button></a></li>
+  <li><a href="assets/page/kids.php" class="<?= basename($_SERVER['PHP_SELF']) == 'kids.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Kids</button></a></li>
+  <li><a href="assets/page/new.php" class="<?= basename($_SERVER['PHP_SELF']) == 'new.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">New</button></a></li>
+  <li><a href="assets/page/women.php" class="<?= basename($_SERVER['PHP_SELF']) == 'women.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Women</button></a></li>
+  <li><a href="assets/page/shopall.php" class="<?= basename($_SERVER['PHP_SELF']) == 'shopall.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Shop All</button></a></li>
   <li><a href="/index.php" class="<?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : '' ?>"><button class="dropdown-item" type="button">Home Page</button></a></li>
 </ul>
